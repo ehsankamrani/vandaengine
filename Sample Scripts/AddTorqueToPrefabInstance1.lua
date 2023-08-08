ab0ea15e701fc4399211022002779761046c495a
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-09 00:41:07 +0330

    script

---
+++ Sample Scripts/AddTorqueToPrefabInstance1.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/addtorquetoprefabinstance/
+
+function Init()
+	AddTorqueToPrefabInstance("1_VandaEngine17-SamplePack1_f1_barrel", 1.0, 1.0, 1.0, 15.0)
+end
+
+function Update()
+
+end
+
+
+
---
+++ Sample Scripts/AddTorqueToPrefabInstance2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/addtorquetoprefabinstance/
+
+--name of the script is addtorquetoprefabinstance2.lua
+function Init()
+	AddTorqueToPrefabInstance("this", 1.0, 0.0, 0.0, 10.0)
+end
+
+function Update()
+
+end
+
+
