0026b3df60b991cc1a6463bd02bf189b83ac8a23
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-08 23:14:28 +0330

    script

---
+++ Sample Scripts/AddForceToPrefabInstance1.lua
@@ -0,0 +1,12 @@
+--Explanations: https://vanda3d.org/addforcetoprefabinstance/
+
+function Init()
+	AddForceToPrefabInstance("1_VandaEngine17-SamplePack1_f1_barrel", 1.0, 1.0, 1.0, 5.0)
+end
+
+function Update()
+
+end
+
+
+
---
+++ Sample Scripts/AddForceToPrefabInstance2.lua
@@ -0,0 +1,13 @@
+--Explanations: https://vanda3d.org/addforcetoprefabinstance/
+
+--name of the script is addforcetoprefabinstance2.lua
+function Init()
+	AddForceToPrefabInstance("this", 1.0, 0.0, 0.0, 4.0)
+end
+
+function Update()
+
+end
+
+
+
