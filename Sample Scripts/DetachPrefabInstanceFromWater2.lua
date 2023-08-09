9a6f67b4bd905f94a0d2e2af6dc5318a6cf87b60
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-09 19:55:49 +0330

    script

---
+++ Sample Scripts/DetachPrefabInstanceFromWater2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/detachprefabinstancefromwater/
+
+--name of script is DetachPrefabInstanceFromWater2.lua
+function Init()
+	DetachPrefabInstanceFromWater("this", "water1")
+end
+
+function Update()
+
+end
+
+
+
---
+++ Sample Scripts/DetachPrefabInstanceFromWater3.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/detachprefabinstancefromwater/
+
+--name of script is DetachPrefabInstanceFromWater3.lua
+function Init()
+	DetachPrefabInstanceFromWater("1_VandaEngine17-SamplePack1_house2", "this")
+end
+
+function Update()
+
+end
+
+
+
