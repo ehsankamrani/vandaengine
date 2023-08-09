52bd6de936d0999408e0d18c275b88cdb4471590
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-09 19:15:54 +0330

    script

---
+++ Sample Scripts/DeleteResource.lua
@@ -0,0 +1,18 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/deleteresource/
+
+function Init()
+	LoadResource("sounds", "mouseHover.ogg")
+	LoadResource("images", "pointer.dds")
+
+	DeleteResource("sounds", "mouseHover.ogg")
+end
+
+function Update()
+
+end
+
+
+
