6658d2fd20dc4eeb0b01a9b2dc09aae1149138ee
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-09 21:06:51 +0330

    script

---
+++ Sample Scripts/DisableDepthOfField.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/disabledepthoffield/
+
+function Init()
+	DisableDepthOfField()
+end
+
+function Update()
+
+end
+
+
+
