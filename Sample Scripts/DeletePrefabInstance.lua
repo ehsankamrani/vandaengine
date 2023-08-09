2bcca1b5c81ed6f6037589384afa14e39117068c
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-09 18:46:25 +0330

    script

---
+++ Sample Scripts/DeletePrefabInstance.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/deleteprefabinstance/
+
+function Init()
+	DeletePrefabInstance("1_VandaEngine17-SamplePack1_stab")
+end
+
+function Update()
+
+end
+
+
+
