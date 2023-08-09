a6e03906c1dd98f87812cbe900b5e42d0e9c4775
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-09 11:17:31 +0330

    script

---
+++ Sample Scripts/CreateFolder.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/createfolder/
+
+function Init()
+	CreateFolder("level1")
+	CreateFolder("level1/subLevel1")
+end
+
+function Update()
+
+end
+
+
+
