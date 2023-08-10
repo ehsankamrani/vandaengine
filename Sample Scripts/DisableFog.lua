69ddd2b76e1c3c7bc5e33f3423d973574a3a88ed
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-10 09:52:47 +0330

    script

---
+++ Sample Scripts/DisableFog.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/disablefog/
+
+function Init()
+	DisableFog()
+end
+
+function Update()
+
+end
+
+
+
