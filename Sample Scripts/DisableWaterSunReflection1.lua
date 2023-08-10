f57e5756403afbd82c59e7487a43a216db4eccf8
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-10 20:35:14 +0330

    script

---
+++ Sample Scripts/DisableWaterSunReflection1.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/disablewatersunreflection/
+
+function Init()
+	DisableWaterSunReflection("water1")
+end
+
+function Update()
+
+end
+
+
+
---
+++ Sample Scripts/DisableWaterSunReflection2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/disablewatersunreflection/
+
+--name of script is DisableWaterSunReflection2.lua
+function Init()
+	DisableWaterSunReflection("this")
+end
+
+function Update()
+
+end
+
+
+
