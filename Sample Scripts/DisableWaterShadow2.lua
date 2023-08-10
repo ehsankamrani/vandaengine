3209e9e5727a2dbfc85d38912533acfc5bf57c7d
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-10 19:28:28 +0330

    script

---
+++ Sample Scripts/DisableWaterShadow2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/disablewatershadow/
+
+--name of script is DisableWaterShadow2.lua
+function Init()
+	DisableWaterShadow("this")
+end
+
+function Update()
+
+end
+
+
+
