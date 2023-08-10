dd45c0f2e908bbe568ad11dbb3f8273975f059eb
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-10 16:23:29 +0330

    script

---
+++ Sample Scripts/DisablePhysicsGravity.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/disablephysicsgravity/
+
+function Init()
+	DisablePhysicsGravity()
+end
+
+function Update()
+
+end
+
+
+
