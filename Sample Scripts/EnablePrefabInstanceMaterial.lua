f2c44911037e71b78f891ed5ee31421542f058d4
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 19:26:44 +0330

    script

---
+++ Sample Scripts/EnablePrefabInstanceMaterial.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enableprefabinstancematerial/
+
+function Init()
+	EnablePrefabInstanceMaterial()
+end
+
+function Update()
+
+end
+
+
+
