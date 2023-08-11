198f175896d220fd023f6196e71718951bc935db
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 21:01:10 +0330

    script

---
+++ Sample Scripts/EnableWaterShadow1.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enablewatershadow/
+
+function Init()
+	EnableWaterShadow("water1")
+end
+
+function Update()
+
+end
+
+
+
---
+++ Sample Scripts/EnableWaterShadow2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enablewatershadow/
+
+--name of script is EnableWaterShadow2.lua
+function Init()
+	EnableWaterShadow("this")
+end
+
+function Update()
+
+end
+
+
+
