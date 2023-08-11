810fe917ed4e3e77edfded18d6d3d490f8c764b6
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 19:16:57 +0330

    script

---
+++ Sample Scripts/EnablePhysicsGroundPlane.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enablephysicsgroundplane/
+
+function Init()
+	EnablePhysicsGroundPlane()
+end
+
+function Update()
+
+end
+
+
+
