5b83e4b84cbfb1258220ac06b221fd97f5cb2176
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 20:13:21 +0330

    script

---
+++ Sample Scripts/EnableSkyFog.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enableskyfog/
+
+function Init()
+	EnableSkyFog()
+end
+
+function Update()
+
+end
+
+
+
