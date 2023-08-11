1513f0b5ab845fdf0f499fbb70d044c51f4924c5
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 19:07:35 +0330

    script

---
+++ Sample Scripts/EnablePhysicsGravity.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enablephysicsgravity/
+
+function Init()
+	EnablePhysicsGravity()
+end
+
+function Update()
+
+end
+
+
+
