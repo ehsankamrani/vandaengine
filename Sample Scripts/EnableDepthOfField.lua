4b77b3e4620a2cbe2f78b4d40471c44c55c80a75
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 14:04:24 +0330

    script

---
+++ Sample Scripts/EnableDepthOfField.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enabledepthOffield/
+
+function Init()
+	EnableDepthOfField()
+end
+
+function Update()
+
+end
+
+
+
