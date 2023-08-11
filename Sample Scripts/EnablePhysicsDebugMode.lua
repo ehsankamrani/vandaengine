8a554997d31c9bc8d498b2f0fa48ecc98dc56eb4
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-11 18:46:30 +0330

    script

---
+++ Sample Scripts/EnablePhysicsDebugMode.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/enablephysicsdebugmode/
+
+function Init()
+	EnablePhysicsDebugMode()
+end
+
+function Update()
+
+end
+
+
+
