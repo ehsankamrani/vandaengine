3c7efa664ccf68a4a09d7d506477d5d7e13d2a8e
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-12 15:29:22 +0330

    script

---
+++ Sample Scripts/ExecuteCyclicAnimation1.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/executecyclicanimation/
+
+function Init()
+	ExecuteCyclicAnimation("1_animation_test_boy", "defaultClip", 1.0, 0.5)
+end
+
+function Update()
+
+end
+
+
+
---
+++ Sample Scripts/ExecuteCyclicAnimation2.lua
@@ -0,0 +1,18 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/executecyclicanimation/
+
+--name of script is executecyclicanimation2.lua
+
+function Init()
+
+end
+
+function Update()
+		ExecuteCyclicAnimation("this", "run", 0.3, 1.0) 
+end
+
+
+
+
