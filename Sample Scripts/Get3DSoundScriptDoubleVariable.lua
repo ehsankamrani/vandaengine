dd64dd49165c0bd163f93834763374d08b67cbb4
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-13 02:48:56 +0330

    string

---
+++ Sample Scripts/Get3DSoundScriptDoubleVariable.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/get3dSoundscriptdoublevariable/
+
+--script name is Get3DSoundScriptDoubleVariable.lua attached a to game object such as water
+return_value = 0.0
+
+function Init()
+	return_value = Get3DSoundScriptDoubleVariable("sound1", "a")
+end
+
+function Update()
+
+end
