f646faae336cfba4f0bb5c723e52901ccb264b35
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-13 03:13:23 +0330

    script

---
+++ Sample Scripts/Get3DSoundScriptIntVariable.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/get3dsoundscriptintvariable/
+
+--script name is Get3DSoundScriptIntVariable.lua attached a to game object such as water
+return_value = 0
+
+function Init()
+	return_value = Get3DSoundScriptIntVariable("sound1", "a")
+end
+
+function Update()
+
+end
