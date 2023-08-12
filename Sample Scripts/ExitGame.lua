24f6580de5625c71729fc3aa63fcfc8940888575
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-12 21:46:06 +0330

    script

---
+++ Sample Scripts/ExitGame.lua
@@ -0,0 +1,20 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/exitgame/
+
+function OnSelectMouseLButtonDown()
+	ExitGame()
+end
+
+function OnSelectMouseRButtonDown()
+
+end
+
+function OnSelectMouseEnter()
+
+end
+
+
+
+
