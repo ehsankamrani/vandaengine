01939c4956ddf1e227d31fe78510d8293771c0e6
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-15 01:40:21 +0330

    script

---
+++ Sample Scripts/GetCharacterControllerPosition.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getcharactercontrollerposition/
+
+x = 0.0
+y = 0.0
+z = 0.0
+
+function Init()
+	x, y, z = GetCharacterControllerPosition()
+end
+
+function Update()
+
+end
