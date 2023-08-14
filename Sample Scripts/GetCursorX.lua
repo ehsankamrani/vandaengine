425e15197559e13481258a7882ddf0b2ed0c114b
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-15 01:50:21 +0330

    script

---
+++ Sample Scripts/GetCursorX.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getcursorx/
+
+x = 0.0
+
+function Init()
+	x = GetCursorX()
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/GetCursorY.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getcursory/
+
+y = 0.0
+
+function Init()
+	y = GetCursorY()
+end
+
+function Update()
+
+end
