bd0c509ed2eb87b8a0b6b2ee3d7a5e9d49e205cc
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-17 02:14:43 +0330

    Create GetGlobalSoundVolume.lua

---
+++ Sample Scripts/GetGlobalSoundVolume.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getglobalsoundvolume/
+
+volume = 0.0
+
+function Init()
+	volume = GetGlobalSoundVolume()
+
+	message = string.format("\nGlobal sound volume is > %.2f" , volume)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
