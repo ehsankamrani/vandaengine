453aba22548b597abd7c03f0618f6369d623e6b2
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-21 07:21:49 +0330

    script

---
+++ Sample Scripts/GetLightSpecular2.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getlightspecular/
+
+--Script name is GetLightSpecular2.lua
+
+red = 0.0
+green = 0.0
+blue = 0.0
+
+function Init()
+		red, green, blue = GetLightSpecular("this")
+
+		message = string.format("\nLight specular color is > (%.2f, %.2f, %.2f)" , red, green, blue)
+		PrintConsole(message)
+end
+
+function Update()
+
+end
+
