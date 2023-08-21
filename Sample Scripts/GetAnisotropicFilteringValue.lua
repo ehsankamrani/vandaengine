6969fa96dd5fa594f8fe2886ec15ed1efffa1a4a
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-22 02:31:44 +0330

    script

---
+++ Sample Scripts/GetAnisotropicFilteringValue.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getanisotropicfilteringvalue/
+
+value = 0
+
+function Init()
+	value = GetAnisotropicFilteringValue()
+
+	message = string.format("\nAnisotropic filtering value is > %d" ,value )
+	PrintConsole(message)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/GetMultisamplingValue.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getmultisamplingvalue/
+
+value = 0
+
+function Init()
+	value = GetMultisamplingValue()
+
+	message = string.format("\nMultisampling value is > %d" ,value )
+	PrintConsole(message)
+end
+
+function Update()
+
+end
