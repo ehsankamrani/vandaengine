7f21f80986c02004c61f61b6ea4e51aaa2ac5b94
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-24 13:29:29 +0330

    script

---
+++ Sample Scripts/GetPhysicsCameraAngle.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getphysicscameraangle/
+
+angle = 0.0
+
+function Init()
+	angle = GetPhysicsCameraAngle()
+
+	message = string.format("\nPhysics camera angle is > %.2f" ,angle)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
