c4919a1dffd18b7f93865e8e30fb92ae5c6403a1
Ehsan Kamrani <opentechno@hotmail.com>
2023-08-28 19:52:56 +0330

    script

---
+++ Sample Scripts/GetSkyPosition.lua
@@ -0,0 +1,19 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getskyposition/
+
+posX = 0.0
+posY = 0.0
+posZ = 0.0
+
+function Init()
+	posX, posY, posZ = GetSkyPosition()
+
+	message = string.format("\nSky position is > (%.2f, %.2f, %.2f)" , posX, posY, posZ)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
