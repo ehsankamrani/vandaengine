94e9a539cb67d3d9edc691ede2e0ec99e3956c2a
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-02 02:27:41 +0330

    script

--- Sample Scripts/GetSoundPlay1.lua
+++ Sample Scripts/GetSoundPlay1.lua
@@ -21,4 +21,3 @@ end
 function Update()
 
 end
-
---
+++ Sample Scripts/GetSoundPosition2.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getsoundposition/
+
+--Name of script is GetSoundPosition2.lua
+
+posX = 0.0
+posY = 0.0
+posZ = 0.0
+
+function Init()
+	posX, posY, posZ = GetSoundPosition("this")
+
+	message = string.format("\nSound position is > (%.2f, %.2f, %.2f)" , posX, posY, posZ)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
