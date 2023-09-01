26afb132a49a0f1d9dae963257efd1cd504c8368
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-02 03:08:24 +0330

    script

---
+++ Sample Scripts/GetSoundReferenceDistance1.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getsoundreferencedistance/
+
+ref_distance = 0
+
+function Init()
+	ref_distance = GetSoundReferenceDistance("sound1")
+
+	message = string.format("\nSound reference distance is > %.2f", ref_distance)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/GetSoundReferenceDistance2.lua
@@ -0,0 +1,20 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getsoundreferencedistance/
+
+--Name of script is GetSoundReferenceDistance2.lua
+
+ref_distance = 0
+
+function Init()
+	ref_distance = GetSoundReferenceDistance("this")
+
+	message = string.format("\nSound reference distance is > %.2f", ref_distance)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
