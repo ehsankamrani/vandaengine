16bfa7a962bee890288784eb9e3ced98b67078f0
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-06 05:47:38 +0330

    script

---
+++ Sample Scripts/GetVideoDuration1.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getvideoduration/
+
+duration = 0.0
+
+function Init()
+	duration = GetVideoDuration("video1")
+
+	message = string.format("\nVideo duration is (%.2f) seconds", duration)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/GetVideoDuration2.lua
@@ -0,0 +1,20 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getvideoduration/
+
+--Name of script is GetVideoDuration2.lua
+
+duration = 0.0
+
+function Init()
+	duration = GetVideoDuration("this")
+
+	message = string.format("\nVideo duration is (%.2f) seconds", duration)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
