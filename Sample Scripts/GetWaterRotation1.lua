16f166f15772fb06ae60a2d38233dd3a87b3528b
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-09 04:04:15 +0330

    script

---
+++ Sample Scripts/GetWaterRotation1.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getwaterrotation/
+
+rotation = 0.0
+
+function Init()
+	rotation = GetWaterRotation("water1")
+
+	message = string.format("\nWater rotation is > %.2f", rotation)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/GetWaterRotation2.lua
@@ -0,0 +1,20 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getwaterrotation/
+
+--Name of script is GetWaterRotation2.lua
+
+rotation = 0.0
+
+function Init()
+	rotation = GetWaterRotation("this")
+
+	message = string.format("\nWater rotation is > %.2f", rotation)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
