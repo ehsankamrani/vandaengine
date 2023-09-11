3d2bba178ef5c39813a5cb20ed3ac45fc30c2962
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-12 03:05:28 +0330

    script

---
+++ Sample Scripts/GetWaterUnderwaterFogDensity1.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getwaterunderwaterfogdensity/
+
+fog_density = 0.0
+
+function Init()
+	fog_density = GetWaterUnderwaterFogDensity("water1")
+
+	message = string.format("\nUnderwater fog density of water is > %.2f", fog_density)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/GetWaterUnderwaterFogDensity2.lua
@@ -0,0 +1,20 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/getwaterunderwaterfogdensity/
+
+--Name of script is GetWaterUnderwaterFogDensity2.lua
+
+fog_density = 0.0
+
+function Init()
+	fog_density = GetWaterUnderwaterFogDensity("this")
+
+	message = string.format("\nUnderwater fog density of water is > %.2f", fog_density)
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
