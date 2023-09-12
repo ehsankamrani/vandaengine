7499cd1b31cb36ad00c64ad7fe705bcd1bc3b096
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-13 03:04:08 +0330

    script

---
+++ Sample Scripts/HideCursorIcon.lua
@@ -0,0 +1,16 @@
+timer = 0.0
+hidden = false
+
+function Init()
+	LoadResource("Images", "Cursor.dds")
+	ShowCursorIcon("Images_Cursor.dds", 5.0)
+end
+
+function Update()
+	if timer < 5.0 then timer = timer + GetElapsedTime() end
+
+	if timer >= 5.0 and not hidden then
+		HideCursorIcon("Images_Cursor.dds")
+		hidden = true
+	end
+end
