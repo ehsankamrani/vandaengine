2299350739e3d4a738279184f902d1dffaacca04
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-13 05:46:34 +0330

    script

---
+++ Sample Scripts/HideGUIButton.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/hideguibutton/
+
+function OnTriggerEnter(otherActorName)
+	HideGUIButton("gui_SampleGUI17_MainMenu", "PlayGame")
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
