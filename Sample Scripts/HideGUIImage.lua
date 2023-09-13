c74f8abcef07e82865543d77fdb0ff797305d4c9
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-13 07:04:19 +0330

    script

---
+++ Sample Scripts/HideGUIImage.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/hideguiimage/
+
+function OnTriggerEnter(otherActorName)
+	HideGUIImage("gui_SampleGUI17_MainMenuAbout", "backgroundImg")
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
