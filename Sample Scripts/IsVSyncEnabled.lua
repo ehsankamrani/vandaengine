50ad83e13f7ae9ceac5ee2a030ee8d86eb1d2725
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-15 13:09:41 +0330

    script

---
+++ Sample Scripts/IsVSyncEnabled.lua
@@ -0,0 +1,25 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/isvsyncenabled/
+
+VSync = false
+message = ""
+
+function Init()
+	VSync = IsVSyncEnabled()
+
+	if VSync then 
+		message = string.format("\nVSync is ON")
+	else
+		message = string.format("\nVSync is OFF")
+	end
+
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
+
