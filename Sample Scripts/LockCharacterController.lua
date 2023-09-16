3c5d0c96817892a210f4f687dd0ec5452e575196
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-16 14:35:09 +0330

    script

---
+++ Sample Scripts/LockCharacterController.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/lockcharactercontroller/
+
+function OnTriggerEnter(otherActorName)
+	LockCharacterController()
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
