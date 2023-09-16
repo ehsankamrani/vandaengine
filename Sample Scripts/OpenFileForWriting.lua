7ecd1b081f1bc041e050f95e3a401997d629c3ec
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-16 15:38:10 +0330

    script

---
+++ Sample Scripts/OpenFileForWriting.lua
@@ -0,0 +1,31 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/openfileforreading/
+
+bVar = false
+fVar = 0.0
+iVar = 0
+sVar = "init"
+
+function Init()
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/level1.bin")
+   WriteBoolVariableToFile(true)
+   WriteFloatVariableToFile(2.0)
+   WriteIntVariableToFile(3)
+   WriteStringVariableToFile("level1")
+   CloseFile("Lev1/level1.bin")
+
+   --Open File to load data
+   OpenFileForReading("Lev1/level1.bin")
+   bVar = ReadBoolVariableFromFile()
+   fVar = ReadFloatVariableFromFile()
+   iVar = ReadIntVariableFromFile()
+   sVar = ReadStringVariableFromFile()
+   CloseFile("Lev1/level1.bin")
+end
+
