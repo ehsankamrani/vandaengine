1bc9775309f6741caf53bbb69b57c500dd7746e7
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-16 12:52:53 +0330

    script

---
+++ Sample Scripts/LoadResource.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/loadresource/
+
+function Init()
+	LoadResource("Images", "Cursor.dds")
+	ShowCursorIcon("Images_Cursor.dds", 5.0)
+end
+
+function Update()
+
+end
+
+
