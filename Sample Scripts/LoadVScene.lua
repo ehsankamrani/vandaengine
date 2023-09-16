77b50785699f7b893fb6acca2865f65f35e65728
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-16 14:02:19 +0330

    script

--- Sample Scripts/LoadVScene.lua
+++ Sample Scripts/LoadVScene.lua
@@ -1,3 +1,8 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/loadvscene/
+
 function OnTriggerEnter(otherActorName)
 	LoadVScene("Sample17Level1")
 end
