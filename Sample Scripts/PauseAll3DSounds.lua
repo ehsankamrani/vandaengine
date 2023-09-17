e30190da6d14ed532978d265c4a4f276d7d9cd31
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-17 06:19:49 +0330

    script

--- Sample Scripts/PauseAll3DSounds.lua
+++ Sample Scripts/PauseAll3DSounds.lua
@@ -1,12 +1,12 @@
 --Copyright (C) 2023 Ehsan Kamrani 
 --This file is licensed and distributed under MIT license
 
---Explanations: https://vanda3d.org/lockcharactercontroller/
+--Explanations: https://vanda3d.org/pauseall3dsounds/
 
 function OnTriggerEnter(otherActorName)
-	--if character controller enters trigger, pause all 3D sounds except a 3D sound named "sound3D_2"
+	--nil means main character controller
 	if otherActorName == nil then
-		PauseAll3DSounds("sound3D_2")
+		PauseAll3DSounds("sound3D_2", "sound3D_3")
 	end
 end
 
