1c3f87ba7ff8563ecf699bfbf573c27e0ccd78b6
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-18 07:44:34 +0330

    script

---
+++ Sample Scripts/PauseAnimationOfAllWaters.lua
@@ -0,0 +1,19 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/pauseanimationofallwaters/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseAnimationOfAllWaters("water2", "water3")
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
