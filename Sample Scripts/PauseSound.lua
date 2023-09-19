c85f9eaa77338b517144dc0363da01ee983b9d5f
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-19 10:29:52 +0330

    script

---
+++ Sample Scripts/PauseSound.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/pausesound/
+
+function Init()
+	PauseSound("this", "ambient2", "fire1")
+end
+
+function Update()
+
+end
