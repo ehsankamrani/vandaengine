7602ed3b955f01768c2dbb5e65bde1ddf4b8a506
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-20 11:30:31 +0330

    script

---
+++ Sample Scripts/PauseUpdateEventOfAll3DSounds.lua
@@ -0,0 +1,19 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/pauseupdateeventofall3dSounds/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseUpdateEventOfAll3DSounds("river2", "river3")
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
