949af85d609c822fbc9d03afb157a8ab8a9214b8
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-20 09:53:21 +0330

    script

---
+++ Sample Scripts/PauseUpdateEventOf3DSound1.lua
@@ -0,0 +1,19 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/pauseupdateeventof3dsound/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseUpdateEventOf3DSound("river1")
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
---
+++ Sample Scripts/PauseUpdateEventOf3DSound2.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/pauseupdateeventof3dsound/
+
+--Name of script is pauseupdateeventof3dsound2.lua
+
+function Init()
+	PauseUpdateEventOf3DSound("this")
+end
+
+function Update()
+
+end
