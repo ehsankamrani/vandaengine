2021807ccb082e20a15e6b84edde7c9bbad75d19
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-21 12:00:12 +0330

    script

---
+++ Sample Scripts/PauseUpdateEventOfLight2.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/pauseupdateeventoflight/
+
+--Name of script is PauseUpdateEventOfLight2.lua
+
+function Init()
+	PauseUpdateEventOfLight("this")
+end
+
+function Update()
+
+end
