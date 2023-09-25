6fc90d7a226e3cebeae90a062976a3b687258607
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-25 16:19:14 +0330

    script

---
+++ Sample Scripts/PlayAllResourceSoundsLoop.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/playallresourcesoundsloop/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		LoadResource("Sounds", "fire.ogg")
+		LoadResource("Sounds", "river.ogg")
+		LoadResource("Sounds", "ambient.ogg")
+	end
+end
+
+function OnTriggerStay(otherActorName)
+end
+
+function OnTriggerExit(otherActorName)
+	if otherActorName == nil then
+		PlayAllResourceSoundsLoop("Sounds_fire.ogg", "Sounds_river.ogg")
+	end
+end
