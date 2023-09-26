110f245befc5f81db2c9920706a23d14f7ded015
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-26 19:46:32 +0330

    script

---
+++ Sample Scripts/PlayAllStopped3DSounds.lua
@@ -0,0 +1,21 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/playallstopped3dsounds/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		StopAll3DSounds()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	if otherActorName == nil then
+		PlayAllStopped3DSounds("river2", "river3")
+	end
+end
