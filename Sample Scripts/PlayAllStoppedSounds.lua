6c4d0762844761905801065f891ef84c0d1f5830
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-27 19:26:58 +0330

    script

---
+++ Sample Scripts/PlayAllStoppedSounds.lua
@@ -0,0 +1,27 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/playallstoppedsounds/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		LoadResource("Sounds", "fire.ogg")
+		LoadResource("Sounds", "river.ogg")
+		LoadResource("Sounds", "ambient.ogg")
+		PlayAllResourceSoundsLoop()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		StopAllSounds()
+	end
+end
+
+function OnTriggerExit(otherActorName)
+	if otherActorName == nil then
+		PlayAllStoppedSounds("ambient2", "river_3D2", "Sounds_fire.ogg", "Sounds_river.ogg")
+	end
+end
