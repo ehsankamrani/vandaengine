fc202dda5d657c45e82c0c38dd69f2b9ea1668de
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-27 23:29:22 +0330

    script

---
+++ Sample Scripts/PlaySoundOnce.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/playsoundonce/
+
+function Init()
+	PlaySoundOnce("this", "river")
+end
+
+function Update()
+
+end
