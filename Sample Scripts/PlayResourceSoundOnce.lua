a7ad26c31e4e00b0b7daddee022f1306cd7270cd
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-27 21:39:15 +0330

    script

---
+++ Sample Scripts/PlayResourceSoundOnce.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/playresourcesoundonce/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		LoadResource("Sounds", "fire.ogg")
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PlayResourceSoundOnce("Sounds_fire.ogg")
+	end
+end
