bead17660bfeb3d91272b4fd80366fceaf7336d7
Ehsan Kamrani <opentechno@hotmail.com>
2023-09-28 16:49:31 +0330

    script

---
+++ Sample Scripts/PrintConsole.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/printconsole/
+
+function Init()
+	message = string.format("\nHello World!")
+	PrintConsole(message)
+end
+
+function Update()
+
+end
+
