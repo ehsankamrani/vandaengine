119aa965dc2848fba743fb0eed1e0d8d922ff759
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-01 18:01:30 +0330

    script

---
+++ Sample Scripts/ReadFloatVariableFromFile.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/readfloatvariablefromfile/
+
+fVar = 0.0
+
+function Init()
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/level1.bin")
+   WriteFloatVariableToFile(2.0)
+   CloseFile("Lev1/level1.bin")
+
+   --Open File to load data
+   OpenFileForReading("Lev1/level1.bin")
+   fVar = ReadFloatVariableFromFile()
+   CloseFile("Lev1/level1.bin")
+end
+
---
+++ Sample Scripts/ReadIntVariableFromFile.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/readintvariablefromFile/
+
+iVar = 0
+
+function Init()
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/level1.bin")
+   WriteIntVariableToFile(3)
+   CloseFile("Lev1/level1.bin")
+
+   --Open File to load data
+   OpenFileForReading("Lev1/level1.bin")
+   iVar = ReadIntVariableFromFile()
+   CloseFile("Lev1/level1.bin")
+end
+
