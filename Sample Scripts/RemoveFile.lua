1c8e98a804f70829f0c5269cbe9013be4cb9c1dd
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-03 02:26:43 +0330

    script

---
+++ Sample Scripts/RemoveFile.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/removefile/
+
+function OnTriggerEnter(otherActorName)
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/level1.bin")
+   WriteBoolVariableToFile(true)
+   CloseFile("Lev1/level1.bin")
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	RemoveFile("Lev1/level1.bin")
+end
