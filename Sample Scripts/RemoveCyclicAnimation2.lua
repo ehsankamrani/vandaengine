bbbb85d9f64f9cb80091525344c0549bf6e50d57
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-02 09:59:50 +0330

    script

---
+++ Sample Scripts/RemoveCyclicAnimation2.lua
@@ -0,0 +1,21 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/removecyclicanimation/
+
+--name of script is RemoveCyclicAnimation2.lua
+
+animation = true
+
+function Init()
+
+end
+
+function Update()
+	if animation == true then 
+		RemoveCyclicAnimation("this", "run", 1.0) 
+		animation = false
+	end
+end
+
+
