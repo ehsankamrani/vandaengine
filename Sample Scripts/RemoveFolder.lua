54eda12e58076fc0774ff8f3162c0b307b15325f
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-03 02:48:06 +0330

    script

---
+++ Sample Scripts/RemoveFolder.lua
@@ -0,0 +1,25 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/removefolder/
+
+function OnTriggerEnter(otherActorName)
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+    --Create a folder in Assets/Data/Lev1 path
+	CreateFolder("Lev1/subLev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/subLev1/level1.bin")
+   WriteBoolVariableToFile(true)
+   CloseFile("Lev1/subLev1/level1.bin")
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	RemoveFolder("Lev1")
+end
