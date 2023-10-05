79e4c81bfce601d310d8a56efe4b15a2618a7cda
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-05 05:44:45 +0330

    script

---
+++ Sample Scripts/ResumeAnimationOfAllWaters.lua
@@ -0,0 +1,21 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeanimationofallwaters/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseAnimationOfAllWaters()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	if otherActorName == nil then
+		ResumeAnimationOfAllWaters("water2", "water3")
+	end
+end
