0f198c7bb24b437eda9f1a1da92d22557685e325
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-06 06:00:37 +0330

    script

---
+++ Sample Scripts/ResumePhysics.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumephysics/
+
+function OnSelectMouseLButtonDown()
+	PausePhysics()
+end
+
+function OnSelectMouseRButtonDown()
+	ResumePhysics()
+end
+
+function OnSelectMouseEnter()
+
+end
+
