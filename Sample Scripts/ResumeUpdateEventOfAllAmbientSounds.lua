69adb4f8cabfc735f5147afe9d5e1a333aaea6bd
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-07 03:19:29 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfAllAmbientSounds.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofallambientsounds/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseUpdateEventOfAllAmbientSounds()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		ResumeUpdateEventOfAllAmbientSounds("ambient2", "ambient3")
+	end
+end
