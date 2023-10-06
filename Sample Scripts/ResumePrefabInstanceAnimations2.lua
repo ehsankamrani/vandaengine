599697843780bb4f3aeb74d4fe135d6bba751a9f
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-06 07:51:17 +0330

    script

---
+++ Sample Scripts/ResumePrefabInstanceAnimations2.lua
@@ -0,0 +1,24 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeprefabinstanceanimations/
+
+--Name of script is ResumePrefabInstanceAnimations2.lua
+
+pause_animation = true
+time = 0.0
+
+function Init()
+	PausePrefabInstanceAnimations("this")
+end
+
+function Update()
+	time = time + GetElapsedTime()
+	if pause_animation and time >= 5.0 then
+		ResumePrefabInstanceAnimations("this")
+		pause_animation = false
+	end
+end
+
+
+
