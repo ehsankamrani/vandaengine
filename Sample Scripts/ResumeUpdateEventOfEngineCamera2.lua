24b1eec9a040ceb77a0fbc8049cc766b69f2bfeb
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-09 07:15:58 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfEngineCamera2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofenginecamera/
+
+--Name of script is ResumeUpdateEventOfEngineCamera2.lua
+
+function Init()
+	PauseUpdateEventOfEngineCamera("this")
+
+	ResumeUpdateEventOfEngineCamera("this")
+end
+
+function Update()
+	PrintConsole("\nUpdate")
+end
