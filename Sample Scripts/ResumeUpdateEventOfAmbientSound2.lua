9a6e9f07e98e5918956cd171dcb4545208a8dbdd
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-09 03:30:53 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfAmbientSound2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofambientsound/
+
+--Name of script is ResumeUpdateEventOfAmbientSound2.lua
+
+function Init()
+	PauseUpdateEventOfAmbientSound("this")
+
+	ResumeUpdateEventOfAmbientSound("this")
+end
+
+function Update()
+	PrintConsole("\nUpdate")
+end
