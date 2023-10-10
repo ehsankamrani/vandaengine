085feb35c3aef4d3e68a38a8309a19ebcbf6d2d3
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-10 11:03:10 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfVSceneScript.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofvscenescript/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseUpdateEventOfVSceneScript()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		ResumeUpdateEventOfVSceneScript()
+	end
+end
