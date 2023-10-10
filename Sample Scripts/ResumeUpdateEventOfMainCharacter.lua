2e1cd80646c91908eb90ea4676cf7548dc4c9ac8
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-10 03:34:47 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfMainCharacter.lua
@@ -0,0 +1,23 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofmaincharacter/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseUpdateEventOfMainCharacter()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		ResumeUpdateEventOfMainCharacter()
+	end
+
+end
