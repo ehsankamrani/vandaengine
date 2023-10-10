3e3ca99fbe67c6b4a9a42ea6ff5860bd9260078b
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-10 07:50:46 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfSky.lua
@@ -0,0 +1,23 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofsky/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		PauseUpdateEventOfSky()
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		ResumeUpdateEventOfSky()
+	end
+
+end
