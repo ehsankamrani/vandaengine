3d93f0d7b328cef58fe11fb08c97c4db82154651
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-10 04:22:48 +0330

    script

---
+++ Sample Scripts/ResumeUpdateEventOfPrefabInstance2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumeupdateeventofprefabinstance/
+
+--Name of script is ResumeUpdateEventOfPrefabInstance2.lua
+
+function Init()
+	PauseUpdateEventOfPrefabInstance("this")
+	ResumeUpdateEventOfPrefabInstance("this")
+end
+
+function Update()
+	PrintConsole("\nUpdate")
+end
