ca86cc11ff3e2a1fa0866255e051b351ee4431cf
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-12 09:00:56 +0330

    script

---
+++ Sample Scripts/RotatePrefabInstance1.lua
@@ -0,0 +1,24 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/rotateprefabinstance/
+
+rotateX = 0.0
+rotateY = 0.0
+rotateZ = 0.0
+
+function Init()
+
+end
+
+function Update()
+	rotateX = rotateX + GetElapsedTime()
+	rotateY = rotateY + (GetElapsedTime() * 2.0)
+	rotateZ = rotateZ + (GetElapsedTime() * 3.0)
+
+	if rotateX > 360.0 then rotateX = rotateX - 360.0 end
+	if rotateY > 360.0 then rotateY = rotateY - 360.0 end
+	if rotateZ > 360.0 then rotateZ = rotateZ - 360.0 end
+
+	RotatePrefabInstance("1_VandaEngine17-SamplePack1_well", rotateX, rotateY, rotateZ)
+end
---
+++ Sample Scripts/RotatePrefabInstance2.lua
@@ -0,0 +1,25 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/rotateprefabinstance/
+
+rotateX = 0.0
+rotateY = 0.0
+rotateZ = 0.0
+
+function Init()
+
+end
+
+function Update()
+	rotateX = rotateX + GetElapsedTime()
+	rotateY = rotateY + (GetElapsedTime() * 2.0)
+	rotateZ = rotateZ + (GetElapsedTime() * 3.0)
+
+	if rotateX > 360.0 then rotateX = rotateX - 360.0 end
+	if rotateY > 360.0 then rotateY = rotateY - 360.0 end
+	if rotateZ > 360.0 then rotateZ = rotateZ - 360.0 end
+
+	RotatePrefabInstance("this", rotateX, rotateY, rotateZ)
+end
+
