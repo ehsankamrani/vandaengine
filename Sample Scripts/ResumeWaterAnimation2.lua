ad62fc600f9ef35d66b40b9a78cc51cab73053f2
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-12 03:37:49 +0330

    script

---
+++ Sample Scripts/ResumeWaterAnimation2.lua
@@ -0,0 +1,19 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/resumewateranimation/
+
+--Name of script is ResumeWaterAnimation2.lua
+
+function Init()
+	PauseWaterAnimation("this")
+
+	ResumeWaterAnimation("this")
+end
+
+function Update()
+
+end
+
+
+
