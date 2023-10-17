07a7473d36479df47ffed7f8dfb9f7354e8472fb
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-17 20:56:15 +0330

    script

---
+++ Sample Scripts/SelectPrefabInstances.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/selectprefabinstances/
+
+function Init()
+	LoadResource("images", "cursor.dds")
+	ShowCursorIcon("images_cursor.dds", 5.0)
+end
+
+function Update()
+	if IsKeyDown("0") then
+		SelectPrefabInstances(GetCursorX(), GetCursorY(), 20.0, 20.0)
+	end
+end
