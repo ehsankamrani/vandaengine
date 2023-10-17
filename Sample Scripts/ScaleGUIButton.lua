f0f7d8bed30ccfb654fc41c3cf654d0569df1c6a
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-17 17:33:14 +0330

    script

---
+++ Sample Scripts/ScaleGUIButton.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/scaleguibutton/
+
+function OnTriggerEnter(otherActorName)
+	ScaleGUIButton("gui_SampleGUI17_MainMenu", "PlayGame", 2.0)
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
---
+++ Sample Scripts/ScaleGUIImage.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/scaleguiimage/
+
+function OnTriggerEnter(otherActorName)
+	ScaleGUIImage("gui_SampleGUI17_MainMenuAbout", "backgroundImg", 1.5)
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
