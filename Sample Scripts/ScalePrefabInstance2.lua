234060f4997ae4e0ad3ada07f698f6dff18dc63c
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-17 18:55:39 +0330

    script

---
+++ Sample Scripts/ScalePrefabInstance2.lua
@@ -0,0 +1,24 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/scaleprefabinstance/
+
+scaleX = 1.0
+scaleY = 1.0
+scaleZ = 1.0
+
+function Init()
+
+end
+
+function Update()
+	scaleX = scaleX + (GetElapsedTime() * 0.1)
+	scaleY = scaleY + (GetElapsedTime() * 0.2)
+	scaleZ = scaleZ + (GetElapsedTime() * 0.3)
+
+	if scaleX > 5.0 then scaleX = 1.0 end
+	if scaleY > 5.0 then scaleY = 1.0 end
+	if scaleZ > 5.0 then scaleZ = 1.0 end
+
+	ScalePrefabInstance("this", scaleX, scaleY, scaleZ)
+end
