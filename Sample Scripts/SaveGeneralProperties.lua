9fbbd5cfc3dcfc229fbd12a0fd9f5a7c80eb4ad4
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-17 15:01:23 +0330

    script

---
+++ Sample Scripts/SaveGeneralProperties.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/savegeneralproperties/
+
+function Init()
+	SaveGeneralProperties()
+end
+
+function Update()
+
+end
+
