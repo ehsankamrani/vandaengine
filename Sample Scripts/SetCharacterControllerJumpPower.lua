94bf54b8131d7878da71b7021cfa4b2df8a28419
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-28 00:52:26 +0330

    script

---
+++ Sample Scripts/SetCharacterControllerJumpPower.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollerjumppower/
+
+function Init()
+	SetCharacterControllerJumpPower(15.0)
+end
+
+function Update()
+
+end
