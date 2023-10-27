2feff9137a7f34e7ba8711608bfe081c848ef5d5
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-28 00:07:45 +0330

    script

---
+++ Sample Scripts/SetCharacterControllerCapsuleRadius.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollercapsuleradius/
+
+function Init()
+	SetCharacterControllerCapsuleRadius(2.0)
+end
+
+function Update()
+
+end
