cddc0d22066eb00edf9503818916ef73f7c58f1e
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-28 00:16:30 +0330

    script

---
+++ Sample Scripts/SetCharacterControllerForcePower.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollerforcepower/
+
+function Init()
+	SetCharacterControllerForcePower(10.0)
+end
+
+function Update()
+
+end
