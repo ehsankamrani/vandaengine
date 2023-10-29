1947e2a900fc2a657f11cc15cd809d66962d5589
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-30 00:50:24 +0330

    script

---
+++ Sample Scripts/SetCharacterControllerRunSpeed.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollerrunspeed/
+
+function Init()
+	SetCharacterControllerRunSpeed(10.0)
+end
+
+function Update()
+
+end
