8c39bc5323e055e9ba04328fa565a7c68317a572
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-30 01:05:35 +0330

    script

---
+++ Sample Scripts/SetCharacterControllerStepOffset.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollerstepoffset/
+
+function Init()
+	SetCharacterControllerStepOffset(0.2)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetCharacterControllerWalkSpeed.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollerwalkspeed/
+
+function Init()
+	SetCharacterControllerWalkSpeed(5.0)
+end
+
+function Update()
+
+end
