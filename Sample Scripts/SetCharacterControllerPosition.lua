2635f204286bf7a028d097b4f264f1a483a074d5
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-30 00:33:15 +0330

    script

---
+++ Sample Scripts/SetCharacterControllerPosition.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setcharactercontrollerposition/
+
+function Init()
+	SetCharacterControllerPosition(2.5, 5.0, 7.0)
+end
+
+function Update()
+
+end
