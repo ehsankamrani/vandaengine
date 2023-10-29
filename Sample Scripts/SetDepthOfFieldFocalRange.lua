a1868e1083629cba9ca42180cd337e9ce8bed6a4
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-30 03:04:25 +0330

    script

---
+++ Sample Scripts/SetDepthOfFieldFocalRange.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdepthoffieldfocalrange/
+
+function Init()
+	SetDepthOfFieldFocalRange(20.0)
+end
+
+function Update()
+
+end
