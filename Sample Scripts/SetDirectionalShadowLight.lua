47d21e999f4423ed3dd12dd0bd471c2487e3b704
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-31 03:10:27 +0330

    script

---
+++ Sample Scripts/SetDirectionalShadowLight.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdirectionalshadowlight/
+
+function Init()
+	SetDirectionalShadowLight("light2")
+end
+
+function Update()
+
+end
