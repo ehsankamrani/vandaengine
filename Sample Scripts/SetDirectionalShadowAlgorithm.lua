68f2d8273463fab4fb7ea5e330628ac2cd152b09
Ehsan Kamrani <opentechno@hotmail.com>
2023-10-31 01:44:59 +0330

    script

---
+++ Sample Scripts/SetDirectionalShadowAlgorithm.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdirectionalshadowalgorithm/
+
+function Init()
+	SetDirectionalShadowAlgorithm("SHADOW_PCF")
+end
+
+function Update()
+
+end
