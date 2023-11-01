620d19888a6409d927a5f78f5b7463629bf8dc2f
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-01 06:15:33 +0330

    script

---
+++ Sample Scripts/SetDirectionalShadowNumberOfSplits.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdirectionalshadownumberofsplits/
+
+function Init()
+	SetDirectionalShadowNumberOfSplits(2)
+end
+
+function Update()
+
+end
