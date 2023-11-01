f9fbb4f4ebe2d0d88a57b9d38a1b065be549f1e1
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-01 06:23:30 +0330

    script

---
+++ Sample Scripts/SetDirectionalShadowResolution.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdirectionalshadowresolution/
+
+function Init()
+	SetDirectionalShadowResolution(1024)
+end
+
+function Update()
+
+end
