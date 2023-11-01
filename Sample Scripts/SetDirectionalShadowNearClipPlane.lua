07f0dfca39d923062a76257678d292f9f749753f
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-01 06:04:23 +0330

    script

---
+++ Sample Scripts/SetDirectionalShadowNearClipPlane.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdirectionalshadownearclipplane/
+
+function Init()
+	SetDirectionalShadowNearClipPlane(0.2)
+end
+
+function Update()
+
+end
