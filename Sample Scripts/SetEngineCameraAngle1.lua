90e79203715b4776d78b532371871075a2f4f511
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-02 04:46:00 +0330

    script

---
+++ Sample Scripts/SetEngineCameraAngle1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameraangle/
+
+function Init()
+	ActivateEngineCamera("camera1")
+	SetEngineCameraAngle("camera1", 30.0)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetEngineCameraAngle2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameraangle/
+
+--Name of script is SetEngineCameraAngle2.lua
+
+function Init()
+	ActivateEngineCamera("this")
+	SetEngineCameraAngle("this", 30.0)
+end
+
+function Update()
+
+end
