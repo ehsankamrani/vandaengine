c7ed7dc6455fdf72bed1b900078ad931bc971730
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-02 03:47:22 +0330

    script

---
+++ Sample Scripts/SetDistanceBetweenPhysicsCameraAndCharacterController.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setdistancebetweenphysicscameraandcharactercontroller/
+
+function Init()
+	SetDistanceBetweenPhysicsCameraAndCharacterController(5.0)
+end
+
+function Update()
+
+end
