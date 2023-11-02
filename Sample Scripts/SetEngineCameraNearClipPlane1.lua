b5915013668351243c435c1f758fd4068690f96b
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-02 05:38:02 +0330

    script

---
+++ Sample Scripts/SetEngineCameraNearClipPlane1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameranearclipplane/
+
+function Init()
+	ActivateEngineCamera("camera1")
+	SetEngineCameraNearClipPlane("camera1", 0.1)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetEngineCameraNearClipPlane2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameranearclipplane/
+
+--Name of script is SetEngineCameraNearClipPlane2.lua
+
+function Init()
+	ActivateEngineCamera("this")
+	SetEngineCameraNearClipPlane("this", 0.1)
+end
+
+function Update()
+
+end
