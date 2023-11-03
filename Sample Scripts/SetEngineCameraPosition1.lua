c758c00f1f30cba5da9911bdb4fcd1a32830f100
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-03 15:14:25 +0330

    script

---
+++ Sample Scripts/SetEngineCameraPosition1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameraposition/
+
+function Init()
+	ActivateEngineCamera("camera1")
+	SetEngineCameraPosition("camera1", 2.5, 5.0, 7.0)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetEngineCameraPosition2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameraposition/
+
+--Name of script is SetEngineCameraPosition2.lua
+
+function Init()
+	ActivateEngineCamera("this")
+	SetEngineCameraPosition("this", 2.5, 5.0, 7.0)
+end
+
+function Update()
+
+end
