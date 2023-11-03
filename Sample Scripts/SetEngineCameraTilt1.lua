e2cb825049c329cbde65fab516dfd2cbc8062468
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-03 17:28:35 +0330

    script

---
+++ Sample Scripts/SetEngineCameraTilt1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameratilt/
+
+function Init()
+	ActivateEngineCamera("camera1")
+	SetEngineCameraTilt("camera1", -20.0)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetEngineCameraTilt2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setenginecameratilt/
+
+--Name of script is SetEngineCameraTilt2.lua
+
+function Init()
+	ActivateEngineCamera("this")
+	SetEngineCameraTilt("this", -20.0)
+end
+
+function Update()
+
+end
