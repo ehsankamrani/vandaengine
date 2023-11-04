0ab7b5ca9f9959ff515e5fcd42caa7adc5995f5b
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-04 14:36:27 +0330

    script

---
+++ Sample Scripts/SetGUIButtonPosition.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setguibuttonposition/
+
+function Init()
+	SetGUIButtonPosition("gui_SampleGUI17_MainMenu", "PlayGame", GetScreenWidth() / 2, GetScreenHeight() / 2)
+end
+
+function Update()
+
+end
