8d8548eba18bf2c73cd8d9b2cec57189f74bedbd
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-05 22:14:03 +0330

    script

---
+++ Sample Scripts/SetGUIImagePosition.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setguiimageposition/
+
+function Init()
+	SetGUIImagePosition("gui_SampleGUI17_MainMenuAbout", "backgroundImg", GetScreenWidth() / 2, GetScreenHeight() / 2)
+end
+
+function Update()
+
+end
