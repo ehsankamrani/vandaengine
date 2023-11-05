d459cdbe284c8576dce103fd4956dda6f1f16a10
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-06 01:03:55 +0330

    script

---
+++ Sample Scripts/SetGUITextPosition.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setguitextposition/
+
+function Init()
+	SetGUITextPosition("gui_SampleGUI17_MainMenu", "text1", GetScreenWidth() / 2, GetScreenHeight() / 2)
+end
+
+function Update()
+
+end
