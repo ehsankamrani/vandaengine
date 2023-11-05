63d4a86147038028712cad0e8e46fe649ce26ff2
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-05 23:39:25 +0330

    script

---
+++ Sample Scripts/SetGUIPosition.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setguiposition/
+
+function Init()
+	SetGUIPosition("gui_SampleGUI17_MainMenu", -5, 10)
+end
+
+function Update()
+
+end
