3bff43c2a2283943700cbae6eb57ce368fd88dac
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-06 22:55:22 +0330

    script

---
+++ Sample Scripts/SetLightDiffuse1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setlightdiffuse/
+
+function Init()
+	SetLightDiffuse("light1", 0.25, 0.5, 0.75)
+end
+
+function Update()
+
+end
+
---
+++ Sample Scripts/SetLightDiffuse2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setlightdiffuse/
+
+--Script name is SetLightDiffuse2.lua
+
+function Init()
+	SetLightDiffuse("this", 0.25, 0.5, 0.75)
+end
+
+function Update()
+
+end
+
