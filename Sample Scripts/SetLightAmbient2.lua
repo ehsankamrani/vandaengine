3e8c5b68d3954d9d491d3181f1c4da3b90e08b4f
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-06 22:51:13 +0330

    script

---
+++ Sample Scripts/SetLightAmbient2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setlightambient/
+
+--Script name is SetLightAmbient2.lua
+
+function Init()
+	SetLightAmbient("this", 0.25, 0.5, 0.75)
+end
+
+function Update()
+
+end
+
