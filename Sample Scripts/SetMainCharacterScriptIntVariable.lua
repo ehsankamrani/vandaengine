4dff06781130d05dff171bdca2feab8ac1ecf940
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-10 15:33:29 +0330

    script

---
+++ Sample Scripts/SetMainCharacterScriptIntVariable.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setmaincharacterscriptintvariable/
+
+--script name is SetMainCharacterScriptIntVariable.lua attached a to game object such as water
+
+function Init()
+	SetMainCharacterScriptIntVariable("a", 1)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetMainCharacterScriptStringVariable.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setmaincharacterscriptstringvariable/
+
+--script name is SetMainCharacterScriptStringVariable.lua attached a to game object such as water
+
+function Init()
+	SetMainCharacterScriptStringVariable("a", "hello")
+end
+
+function Update()
+
+end
