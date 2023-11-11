b93f092eb0ab79d91e5ccf390caebaa588246842
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-11 17:36:01 +0330

    script

---
+++ Sample Scripts/SetPhysicGroundHeight.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicgroundheight/
+
+function Init()
+	SetPhysicGroundHeight(-2.1)
+end
+
+function Update()
+
+end
