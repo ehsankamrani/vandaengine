dcca0916808be0f25f4e39a9d11d7e707f22c24b
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-12 14:20:15 +0330

    script

---
+++ Sample Scripts/SetPhysicsCollisionFlags1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicscollisionflags/
+
+function Init()
+	SetPhysicsCollisionFlags("DYNAMIC", "DYNAMIC", false)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetPhysicsCollisionFlags3.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicscollisionflags/
+
+function Init()
+	SetPhysicsCollisionFlags("DYNAMIC", "KINEMATIC", true)
+end
+
+function Update()
+
+end
