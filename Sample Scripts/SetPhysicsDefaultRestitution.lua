0bba8f959fd9bc435adf90785cf50c7ea4f2f5ad
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-12 21:04:42 +0330

    script

---
+++ Sample Scripts/SetPhysicsDefaultRestitution.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicsdefaultrestitution/
+
+function Init()
+	SetPhysicsDefaultRestitution(0.8)
+end
+
+function Update()
+
+end
