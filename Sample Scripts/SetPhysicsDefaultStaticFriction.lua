b4b00970b24c033dbc79e9f8cfca0bf89d458807
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-13 00:08:36 +0330

    script

---
+++ Sample Scripts/SetPhysicsDefaultStaticFriction.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicsdefaultstaticfriction/
+
+function Init()
+	SetPhysicsDefaultStaticFriction(0.1)
+end
+
+function Update()
+
+end
