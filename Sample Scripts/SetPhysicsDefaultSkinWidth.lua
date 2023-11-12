e489aa88e16407f7969163b3821cb1b265e63065
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-12 23:19:29 +0330

    script

---
+++ Sample Scripts/SetPhysicsDefaultSkinWidth.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicsdefaultskinwidth/
+
+function Init()
+	SetPhysicsDefaultSkinWidth(0.3)
+end
+
+function Update()
+
+end
