00618fac20adb49e10cd92f17a04551ae07f6cc9
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-13 00:16:42 +0330

    script

---
+++ Sample Scripts/SetPhysicsGravity.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setphysicsgravity/
+
+function Init()
+	SetPhysicsGravity(-1.3, -6.8, -1.1)
+end
+
+function Update()
+
+end
