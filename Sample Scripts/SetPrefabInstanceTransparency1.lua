7bcf995a69ae4d58e9c0dcb1d3594b2a495eff8e
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-15 23:22:41 +0330

    script

---
+++ Sample Scripts/SetPrefabInstanceTransparency1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setprefabinstancetransparency/
+
+function Init()
+	EnablePrefabInstanceMaterial("1_VandaEngine17-SamplePack1_f1_barrel")
+	SetPrefabInstanceTransparency("1_VandaEngine17-SamplePack1_f1_barrel", 0.5)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetPrefabInstanceTransparency2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setprefabinstancetransparency/
+
+--Script name is SetPrefabInstanceTransparency2.lua
+
+function Init()
+	EnablePrefabInstanceMaterial("this")
+	SetPrefabInstanceTransparency("this", 0.5)
+end
+
+function Update()
+
+end
