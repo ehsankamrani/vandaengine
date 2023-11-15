05bf40a8461b6d65e502c60f79c5d7d8482c7b8f
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-15 07:24:22 +0330

    script

--- Sample Scripts/SetPrefabInstanceDiffuse2.lua
+++ Sample Scripts/SetPrefabInstanceDiffuse2.lua
@@ -1,13 +1,13 @@
 --Copyright (C) 2023 Ehsan Kamrani 
 --This file is licensed and distributed under MIT license
 
---Explanations: https://vanda3d.org/setprefabinstanceambient/
+--Explanations: https://vanda3d.org/setprefabinstancediffuse/
 
---Script name is SetPrefabInstanceAmbient2.lua
+--Script name is SetPrefabInstanceDiffuse2.lua
 
 function Init()
 	EnablePrefabInstanceMaterial("this")
-	SetPrefabInstanceAmbient("this", 0.75, 0.5, 0.25)
+	SetPrefabInstanceDiffuse("this", 0.75, 0.5, 0.25)
 end
 
 function Update()
