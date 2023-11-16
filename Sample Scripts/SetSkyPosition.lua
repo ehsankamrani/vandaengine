13e705c50a3bcb63c79b4093670141d2957fdb20
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-16 21:49:04 +0330

    script

---
+++ Sample Scripts/SetSkyPosition.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setskyposition/
+
+function Init()
+	SetSkyPosition(2.5, 5.0, 7.5)
+end
+
+function Update()
+
+end
