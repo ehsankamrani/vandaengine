bfffafcb2731c2c74c6cc4da8796a56d9295fa86
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-16 20:10:55 +0330

    script

---
+++ Sample Scripts/SetScreenResolution.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setscreenresolution/
+
+function Init()
+	SetScreenResolution(1920)
+end
+
+function Update()
+
+end
