862129335cb224ef010fee65e39f5e709bd83ced
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-18 10:07:02 +0330

    script

---
+++ Sample Scripts/SetSoundLoop1.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundloop/
+
+function Init()
+	SetSoundLoop("sound1", false)
+	PlaySound("sound1")
+end
+
+function Update()
+
+end
+
---
+++ Sample Scripts/SetSoundLoop2.lua
@@ -0,0 +1,17 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundloop/
+
+--Name of script is SetSoundLoop2.lua
+
+function Init()
+	SetSoundLoop("this", true)
+	PlaySound("this")
+end
+
+function Update()
+
+end
+
+
