f2e86cbdd0154431e162e795c7e4db8e55e775a0
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-18 21:45:53 +0330

    script

---
+++ Sample Scripts/SetSoundReferenceDistance1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundreferencedistance/
+
+function Init()
+	SetSoundReferenceDistance("sound1", 4.5)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetSoundReferenceDistance2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundreferencedistance/
+
+--Name of script is SetSoundReferenceDistance2.lua
+
+function Init()
+	SetSoundReferenceDistance("this", 5.0)
+end
+
+function Update()
+
+end
+
