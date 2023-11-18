8b47d39cf0b3af3604eb71bb5dbb30e4282b15f1
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-18 11:40:46 +0330

    script

---
+++ Sample Scripts/SetSoundMaxDistance1.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundmaxdistance/
+
+function Init()
+	SetSoundMaxDistance("sound1", 1.7)
+end
+
+function Update()
+
+end
+
---
+++ Sample Scripts/SetSoundMaxDistance2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundmaxdistance/
+
+--Name of script is SetSoundMaxDistance2.lua
+
+function Init()
+	SetSoundMaxDistance("this", 2.5)
+end
+
+function Update()
+
+end
+
