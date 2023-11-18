e6a86c2058dc8b19b3d6ed4e1ae9c7ee378ba109
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-18 12:16:59 +0330

    script

---
+++ Sample Scripts/SetSoundPitch1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundpitch/
+
+function Init()
+	SetSoundPitch("sound1", 1.5)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetSoundPitch2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundpitch/
+
+--Name of script is SetSoundPitch2.lua
+
+function Init()
+	SetSoundPitch("this", 0.5)
+end
+
+function Update()
+
+end
+
