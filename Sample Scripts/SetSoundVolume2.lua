8528d16097822d382e725becc449d93dfae3b587
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-19 02:02:55 +0330

    script

---
+++ Sample Scripts/SetSoundVolume2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundvolume/
+
+--Name of script is SetSoundVolume2.lua
+
+function Init()
+	SetSoundVolume("this", 0.2)
+end
+
+function Update()
+
+end
+
