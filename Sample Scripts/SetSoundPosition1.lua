12f7ea033bd3138c9bbb6f6c1af635323380c8dd
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-18 20:58:04 +0330

    script

---
+++ Sample Scripts/SetSoundPosition1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundposition/
+
+function Init()
+	SetSoundPosition("sound1", 2.5, 5.0, 7.0)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetSoundPosition2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setsoundposition/
+
+--Name of script is SetSoundPosition2.lua
+
+function Init()
+	SetSoundPosition("this", 2.5, 5.0, 7.0)
+end
+
+function Update()
+
+end
+
