77f25510bf8fb21944ee6d49c876a9c5b337c80e
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-19 23:37:22 +0330

    script

---
+++ Sample Scripts/SetTerrainDiffuse.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setterraindiffuse/
+
+function Init()
+	SetTerrainDiffuse(0.25, 0.5, 0.75)
+end
+
+function Update()
+
+end
+
+
