a70ed834e3040e891f777ea54cd2953d8d24faa0
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-21 23:26:25 +0330

    script

---
+++ Sample Scripts/SetTerrainShininess.lua
@@ -0,0 +1,13 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setterrainshininess/
+
+function Init()
+	SetTerrainShininess(50.0)
+end
+
+function Update()
+
+end
+
