18c6dcf9f0dd5e6fb92b3f19d250cde56f84910a
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-23 14:11:22 +0330

    script

---
+++ Sample Scripts/SetVideoLoop1.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setvideoloop/
+
+function Init()
+	SetVideoLoop("video1", true)
+	PlayVideo("video1")
+end
+
+function Update()
+
+end
+
---
+++ Sample Scripts/SetVideoLoop2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setvideoloop/
+
+--Name of script is SetVideoLoop2.lua
+
+function Init()
+	SetVideoLoop("this", false)
+	PlayVideo("this")
+end
+
+function Update()
+
+end
+
