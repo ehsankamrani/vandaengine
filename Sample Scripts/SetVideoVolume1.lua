0f8ca40404e8a1f4b17c790f647a4ec00533a358
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-24 03:00:44 +0330

    script

---
+++ Sample Scripts/SetVideoVolume1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setvideovolume/
+
+function Init()
+	SetVideoVolume("video1", 0.1)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetVideoVolume2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setvideovolume/
+
+--Name of script is SetVideoVolume2.lua
+
+function Init()
+	SetVideoVolume("this", 0.35)
+end
+
+function Update()
+
+end
+
