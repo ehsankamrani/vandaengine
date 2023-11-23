9577ad58aa8cb29fa693dc10fe0ee90408fec9ae
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-23 10:39:37 +0330

    script

---
+++ Sample Scripts/SetTriggerScriptIntVariable.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/settriggerscriptintvariable/
+
+--script name is SetTriggerScriptIntVariable.lua attached a to game object such as water
+
+function Init()
+	SetTriggerScriptIntVariable("trigger1", "a", 1)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetTriggerScriptStringVariable.lua
@@ -0,0 +1,14 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/settriggerscriptstringvariable/
+
+--script name is SetTriggerScriptStringVariable.lua attached a to game object such as water
+
+function Init()
+	SetTriggerScriptStringVariable("trigger1", "a", "hello")
+end
+
+function Update()
+
+end
