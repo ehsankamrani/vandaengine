31b29769b9078af6b515d5c822f7b2e2c3084932
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-25 02:36:10 +0330

    script

---
+++ Sample Scripts/SetWaterInvisible1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterinvisible/
+
+function Init()
+	SetWaterInvisible("water1")
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterInvisible2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterinvisible/
+
+--Name of script is SetWaterInvisible2.lua
+
+function Init()
+	SetWaterInvisible("this")
+end
+
+function Update()
+
+end
+
