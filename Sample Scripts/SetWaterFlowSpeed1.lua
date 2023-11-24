616ea7ff8b8fd19e88c42e0ab31c3640cd575701
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-25 02:24:00 +0330

    script

---
+++ Sample Scripts/SetWaterFlowSpeed1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterflowspeed/
+
+function Init()
+	SetWaterFlowSpeed("water1", 1.1)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterFlowSpeed2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterflowspeed/
+
+--Name of script is SetWaterFlowSpeed2.lua
+
+function Init()
+	SetWaterFlowSpeed("this", -0.05)
+end
+
+function Update()
+
+end
+
