290da557656f9052634a633cad00d398a77342ed
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-25 06:59:59 +0330

    script

---
+++ Sample Scripts/SetWaterScale1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterscale/
+
+function Init()
+	SetWaterScale("water1", 11.5, 23.5)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterScale2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterscale/
+
+--Name of script is SetWaterScale2.lua
+
+function Init()
+	SetWaterScale("this", 2.5, 14.2)
+end
+
+function Update()
+
+end
+
