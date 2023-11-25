39448a96ed3e61d5e25da51fbb5ddeaf1cecd5a4
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-25 03:35:45 +0330

    script

---
+++ Sample Scripts/SetWaterLightPosition1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterlightposition/
+
+function Init()
+	SetWaterLightPosition("water1", -14.5, 2.7, 23.0)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterLightPosition2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterlightposition/
+
+--Name of script is SetWaterLightPosition2.lua
+
+function Init()
+	SetWaterLightPosition("this", 23.0, 3.5, 27.2)
+end
+
+function Update()
+
+end
+
