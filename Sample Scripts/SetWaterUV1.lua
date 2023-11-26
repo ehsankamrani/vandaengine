1bee034898673fa76d873bd3fc0086e6f5f260c0
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-26 09:28:27 +0330

    script

---
+++ Sample Scripts/SetWaterUV1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwateruv/
+
+function Init()
+	SetWaterUV("water1", 0.5)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterUV2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwateruv/
+
+--Name of script is SetWaterUV2.lua
+
+function Init()
+	SetWaterUV("this", 6.5)
+end
+
+function Update()
+
+end
+
