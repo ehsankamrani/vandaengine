2de1ae315c82b78b7b00d3820689ffd7585d9ff7
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-26 09:47:13 +0330

    script

---
+++ Sample Scripts/SetWaterVisible1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwatervisible/
+
+function Init()
+	SetWaterVisible("water1")
+end
+
+function Update()
+
+end
