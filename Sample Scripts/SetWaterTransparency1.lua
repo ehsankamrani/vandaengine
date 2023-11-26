48f8ce424933d01938a663862aa0dc101a216fa2
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-26 06:05:23 +0330

    script

---
+++ Sample Scripts/SetWaterTransparency1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwatertransparency/
+
+function Init()
+	SetWaterTransparency("water1", 0.4)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterTransparency2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwatertransparency/
+
+--Name of script is SetWaterTransparency2.lua
+
+function Init()
+	SetWaterTransparency("this", 0.34)
+end
+
+function Update()
+
+end
+
