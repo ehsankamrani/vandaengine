0b21499fd140b33a60b2aef4724ba8949a783a24
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-26 06:28:50 +0330

    script

---
+++ Sample Scripts/SetWaterUnderwaterColor1.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterunderwatercolor/
+
+function Init()
+	SetWaterUnderwaterColor("water1", 0.25, 0.5, 0.75)
+end
+
+function Update()
+
+end
---
+++ Sample Scripts/SetWaterUnderwaterColor2.lua
@@ -0,0 +1,15 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/setwaterunderwatercolor/
+
+--Name of script is SetWaterUnderwaterColor2.lua
+
+function Init()
+	SetWaterUnderwaterColor("this", 0.25, 0.5, 0.75)
+end
+
+function Update()
+
+end
+
