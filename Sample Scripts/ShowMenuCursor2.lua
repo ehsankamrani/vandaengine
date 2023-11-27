b5caba13cd15b79627fbd357b5976e71394f6412
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-27 08:45:23 +0330

    script

---
+++ Sample Scripts/ShowMenuCursor2.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/showmenucursor/
+
+function OnTriggerEnter(otherActorName)
+	ShowMenuCursor()
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	HideMenuCursor()
+end
