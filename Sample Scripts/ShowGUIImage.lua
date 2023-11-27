30bd64fb2bd0e087d5512e9800454f82e957275f
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-27 08:03:53 +0330

    script

---
+++ Sample Scripts/ShowGUIImage.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/showguiimage/
+
+function OnTriggerEnter(otherActorName)
+	HideGUIImage("gui_SampleGUI17_MainMenuAbout", "backgroundImg")
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	ShowGUIImage("gui_SampleGUI17_MainMenuAbout", "backgroundImg")
+end
