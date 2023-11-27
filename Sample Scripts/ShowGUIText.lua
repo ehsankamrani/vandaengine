9adf72bd8e60351f20d6846b05c7549916f460f8
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-27 08:11:50 +0330

    script

---
+++ Sample Scripts/ShowGUIText.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/showguitext/
+
+function OnTriggerEnter(otherActorName)
+	HideGUIText("gui_SampleGUI17_MainMenuAbout", "text1")
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+	ShowGUIText("gui_SampleGUI17_MainMenuAbout", "text1")
+end
