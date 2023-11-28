ceb69260aeadd9e5b93b9e245097e394550412cd
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-28 10:35:51 +0330

    script

---
+++ Sample Scripts/StopAll3DSounds.lua
@@ -0,0 +1,19 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/stopall3dsounds/
+
+function OnTriggerEnter(otherActorName)
+	--nil means main character controller
+	if otherActorName == nil then
+		StopAll3DSounds("sound3D_2", "sound3D_3")
+	end
+end
+
+function OnTriggerStay(otherActorName)
+
+end
+
+function OnTriggerExit(otherActorName)
+
+end
