f97fdbbdc820c10c449505ba906e873fafed421e
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-29 07:14:30 +0330

    script

---
+++ Sample Scripts/StopSound.lua
@@ -0,0 +1,12 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/stopsound/
+
+function Init()
+	StopSound("this", "ambient2", "fire1")
+end
+
+function Update()
+
+end
