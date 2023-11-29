6caf6c5ba6758c4190289f7c9b2262364a11ecf0
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-29 15:52:14 +0330

    script

---
+++ Sample Scripts/WriteBoolVariableToFile.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/writeboolvariabletofile/
+
+bVar = false
+
+function Init()
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/level1.bin")
+   WriteBoolVariableToFile(true)
+   CloseFile("Lev1/level1.bin")
+
+   --Open File to load data
+   OpenFileForReading("Lev1/level1.bin")
+   bVar = ReadBoolVariableFromFile()
+   CloseFile("Lev1/level1.bin")
+end
+
---
+++ Sample Scripts/WriteStringVariableToFile.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/writestringvariabletofile/
+
+sVar = "init"
+
+function Init()
+   --Create a folder in Assets/Data/ path
+   CreateFolder("Lev1")
+
+   --Create and open file to write data
+   OpenFileForWriting("Lev1/level1.bin")
+   WriteStringVariableToFile("level1")
+   CloseFile("Lev1/level1.bin")
+
+   --Open File to load data
+   OpenFileForReading("Lev1/level1.bin")
+   sVar = ReadStringVariableFromFile()
+   CloseFile("Lev1/level1.bin")
+end
+
