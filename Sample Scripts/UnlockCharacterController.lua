8d3fd687bd18034a1ed15914cecd1cfb24015766
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-29 15:37:17 +0330

    script

---
+++ Sample Scripts/UnlockCharacterController.lua
@@ -0,0 +1,16 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/unlockcharactercontroller/
+
+function OnSelectMouseLButtonDown()
+	LockCharacterController()
+end
+
+function OnSelectMouseRButtonDown()
+	UnlockCharacterController()
+end
+
+function OnSelectMouseEnter()
+
+end
