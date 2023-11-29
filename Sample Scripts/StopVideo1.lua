08cec8593bef5fe96e42b01dcaca9ee18fbbcd17
Ehsan Kamrani <opentechno@hotmail.com>
2023-11-29 07:49:46 +0330

    script

---
+++ Sample Scripts/StopVideo1.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/stopvideo/
+
+--Name of script is StopVideo1.lua
+--You can attach this script to a video object
+
+timer = 0.0
+stop = false
+
+function Init()
+	PlayVideo("this")
+end
+
+function Update()
+	timer = timer + GetElapsedTime()
+	if timer >= (GetVideoDuration("this") / 5.0) and not stop then
+		StopVideo("this")
+		stop = true
+	end
+end
---
+++ Sample Scripts/StopVideo2.lua
@@ -0,0 +1,22 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/stopvideo/
+
+--You can attach this script to a video object named "video1"
+
+
+timer = 0.0
+stop = false
+
+function Init()
+	PlayVideo("video1")
+end
+
+function Update()
+	timer = timer + GetElapsedTime()
+	if timer >= (GetVideoDuration("video1") / 5.0) and not stop then
+		StopVideo("video1")
+		stop = true
+	end
+end
