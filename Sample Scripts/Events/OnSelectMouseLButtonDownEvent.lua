0137b7d78cc62268f67d77eb39381a5d697023b1
Ehsan Kamrani <opentechno@hotmail.com>
2023-12-02 13:30:18 +0330

    script

---
+++ Sample Scripts/Events/OnSelectMouseLButtonDownEvent.lua
@@ -0,0 +1,8 @@
+--Copyright (C) 2023 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/onselectmouselbuttondownevent/
+
+function OnSelectMouseLButtonDown()
+	PrintConsole("\nOnSelectMouseLButtonDown() Event was called")
+end
