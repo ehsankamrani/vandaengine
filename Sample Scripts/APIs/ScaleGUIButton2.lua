4314f737745bb52c4b346af9b5e2941189228d26
Ehsan Kamrani <opentechno@hotmail.com>
2023-12-27 23:35:36 +0330

    script

---
+++ Sample Scripts/APIs/ScaleGUIButton2.lua
@@ -0,0 +1,18 @@
+--Copyright (C) 2024 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/scaleguibutton/
+
+--Name of script is ScaleGUIButton2.lua
+
+function OnSelectMouseLButtonDown()
+	ScaleGUIButton("this", "PlayGame", 2.0)
+end
+
+function OnSelectMouseRButtonDown()
+
+end
+
+function OnSelectMouseEnter()
+
+end
