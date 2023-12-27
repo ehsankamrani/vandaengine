9496734aa88d7913bb11050ae654106417ede3f7
Ehsan Kamrani <opentechno@hotmail.com>
2023-12-28 00:04:46 +0330

    script

---
+++ Sample Scripts/APIs/ScaleGUIImage2.lua
@@ -0,0 +1,18 @@
+--Copyright (C) 2024 Ehsan Kamrani 
+--This file is licensed and distributed under MIT license
+
+--Explanations: https://vanda3d.org/scaleguiimage/
+
+--Name of script is ScaleGUIImage2.lua
+
+function OnSelectMouseLButtonDown()
+	ScaleGUIImage("this", "BackgroundImg", 2.0)
+end
+
+function OnSelectMouseRButtonDown()
+
+end
+
+function OnSelectMouseEnter()
+
+end
